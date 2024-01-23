5d306fb3d665859d61630737ee2a26060a408ec2
Dmitry Vlasenko <57891870+Dmitry-Vlasenko@users.noreply.github.com>
2024-01-23 14:18:56 +0200

    Implementation converting kilometers to miles

--- src/app/Converter.java
+++ src/app/Converter.java
@@ -7,6 +7,7 @@ public class Converter {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Select an operation");
         System.out.println("1) Converting miles to kilometers");
+        System.out.println("2) Converting kilometers to miles");
         System.out.println("3) Exit converter");
 
         while(true){
@@ -16,20 +17,28 @@ public class Converter {
                 break;
             } else if (operation.equals(1)) {
                 System.out.println("Write count miles for converting to kilometers");
-                Integer data = scanner.nextInt();
+                double data = scanner.nextDouble();
                 double result = calculation(data, operation);
                 System.out.println(result + " kilometers");
+            } else if (operation.equals(2)) {
+                System.out.println("Write count kilometers for converting to miles");
+                double data = scanner.nextDouble();
+                double result = calculation(data, operation);
+                System.out.println(result + " miles");
             } else {
                 System.out.println("Incorrect operation");
             }
+
         }
         scanner.close();
     }
 
-    private static double calculation(int data, int operation) {
+    private static double calculation(double data, int operation) {
         switch (operation) {
             case 1:
                 return data * 1.609344;
+            case 2:
+                return data / 1.609344;
             default:
                 throw new IllegalArgumentException("Incorrect operation");
         }
