9c89bc0053359420f64e8c1b2f4a31331c39e966
Dmitry Vlasenko <57891870+Dmitry-Vlasenko@users.noreply.github.com>
2024-01-25 14:57:40 +0200

    change if to case

--- src/app/Converter.java
+++ src/app/Converter.java
@@ -12,26 +12,35 @@ public class Converter {
         System.out.println("Select an operation \n 1) Converting miles to kilometers \n 2) Converting kilometers to miles \n 3) Exit converter");
 
         while (true) {
-            Integer operation = scanner.nextInt();
-            if (operation.equals(3)) {
-                System.out.println("Exiting converter. Goodbye!");
-                break;
-            } else if (operation.equals(1)) {
-                System.out.println("Write count miles for converting to kilometers");
-                double data = scanner.nextDouble();
-                double result = calculation(data, operation);
-                System.out.println(result + " kilometers");
-            } else if (operation.equals(2)) {
-                System.out.println("Write count kilometers for converting to miles");
-                double data = scanner.nextDouble();
-                double result = calculation(data, operation);
-                System.out.println(result + " miles");
-            } else {
-                System.out.println("Incorrect operation");
+            try {
+                int operation = scanner.nextInt();
+                switch (operation) {
+                    case 1: {
+                        System.out.println("Write count miles for converting to kilometers");
+                        double data = scanner.nextDouble();
+                        double result = calculation(data, operation);
+                        System.out.println(result + " kilometers");
+                    }
+                    case 2: {
+                        System.out.println("Write count kilometers for converting to miles");
+                        double data = scanner.nextDouble();
+                        double result = calculation(data, operation);
+                        System.out.println(result + " miles");
+                    }
+                    case 3: {
+                        System.out.println("Exiting converter. Goodbye!");
+                        break;
+                    }
+                    default: {
+                        System.out.println("Incorrect operation");
+                    }
+                }
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a number.");
+                scanner.nextLine();
             }
-
+            scanner.close();
         }
-        scanner.close();
     }
 
     private static double calculation(double data, int operation) {
